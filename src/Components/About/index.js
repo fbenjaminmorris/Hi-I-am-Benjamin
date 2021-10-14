03a6641fc58199c6c4d0baa1ddd3623ab4d3e393
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-10-14 16:38:52 -0400

    update

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import coverImage from '../../assets/cover/cover-image.jpg';
+import coverImage from '../../assets/photography/1.jpg';
 function About() {
   return (
     <section className="my-5">
